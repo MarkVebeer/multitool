a35a090a43fc97742738cc44be47f6bc467ba502
Véber-Jurassa Márk <markveber100@gmail.com>
2024-08-11 07:04:07 +0200

    separate script

---
+++ public/script.js
@@ -0,0 +1,25 @@
+document.getElementById('shortenBtn').addEventListener('click', async () => {
+    const urlInput = document.getElementById('urlInput').value;
+    const result = document.getElementById('result');
+
+    if (!urlInput) {
+        result.textContent = 'Please enter a URL';
+        return;
+    }
+
+    const response = await fetch('/shorten', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ originalUrl: urlInput })
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        result.textContent = `Short URL: ${data.shortUrl} copied to clipboard!`;
+        navigator.clipboard.writeText(data.shortUrl)
+    } else {
+        result.textContent = 'Error creating short URL';
+    }
+});
